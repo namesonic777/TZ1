9df1c9a632430db4fff5eb9650b8144d62536bcd
Sofya M <167525854+namesonic777@users.noreply.github.com>
2024-05-01 19:10:33 +0300

    scratch.sh

--- scratch.sh
+++ scratch.sh
@@ -2,7 +2,7 @@
 # Задаем входную и выходную директории из переданных аргументов
 input_directory=$1
 output_directory=$2
-if [ ! -d "$output_directory" ]; then # Проверяем, существует ли выходная директория, и если нет, то создаем ее
+if [[ ! -d "$output_directory" ]]; then # Проверяем, существует ли выходная директория, и если нет, то создаем ее
     mkdir -p "$output_directory"
 fi
 copy_and_rename_files() { # Функция для копирования и переименования файлов
@@ -10,14 +10,14 @@ copy_and_rename_files() { # Функция для копирования и пе
     local dest_dir="$2"
     local files=("$src_dir"/*)
     for file in "${files[@]}"; do # Перебираем файлы в исходной директории
-        if [ -d "$file" ]; then # Если это директория, рекурсивно вызываем функцию для копирования вложенных файлов
+        if [[ -d "$file" ]]; then # Если это директория, рекурсивно вызываем функцию для копирования вложенных файлов
             copy_and_rename_files "$file" "$dest_dir"
-        elif [ -f "$file" ]; then # Если это файл, определяем его имя и путь в выходной директории
+        elif [[ -f "$file" ]]; then # Если это файл, определяем его имя и путь в выходной директории
             filename=$(basename -- "$file")
             dest_file="$dest_dir/${filename}"
-            if [ -f "$dest_file" ]; then # Если файл с таким именем уже существует, добавляем числовой суффикс к имени
+            if [[ -f "$dest_file" ]]; then # Если файл с таким именем уже существует, добавляем числовой суффикс к имени
                 i=1
-                while [ -f "$dest_dir/($i) ${filename}" ]; do
+                while [[ -f "$dest_dir/($i) ${filename}" ]]; do
                     ((i++))
                 done
                 cp "$file" "$dest_dir/($i) ${filename} "
